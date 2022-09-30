d8a63d7ebd744915bcd8c176b7fd8d6a9b3ba742
Kristoffer Ekendahl <kristoffer.ekendahl@iveo.se>
2022-09-30 17:59:46 +0200

    Fixes to Matchinput

--- src/components/MatchInput.vue
+++ src/components/MatchInput.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
-    <input v-model="colorStore.matchColor" type="text">
+    <input v-model="textInput" type="text">
     <input v-model="colorStore.matchColor" type="color">
   </div>
 </template>
 
 <script setup lang="ts">
 import { useColorStore } from '../store/colorStore'
+import { ref, watch } from 'vue'
+import { storeToRefs } from 'pinia'
 const colorStore = useColorStore()
+
+const textInput = ref('')
+
+const { matchColor } = storeToRefs(colorStore)
+
+// Changes from text input to state
+watch(textInput, (input) => {
+  input = input.trim().toLowerCase()
+  /**
+   * @todo - shorthand
+   */
+  const regex = new RegExp('#[0-9a-fA-F]{6}')
+  //const regex = new RegExp('#[0-9a-f]{6}|#[0-9a-f]{3}')
+  if (input[0] !== '#') {
+    input = '#' + input
+  }
+  if (regex.test(input)) {
+    colorStore.matchColor = input
+  }
+})
+
+// changes from state to textinput
+watch(matchColor, (newMatchColor) => {
+  if (newMatchColor !== textInput.value) {
+    textInput.value = newMatchColor
+  }
+})
+
 </script>
 
 <style scoped lang="postcss">
--- src/store/colorStore.ts
+++ src/store/colorStore.ts
@@ -13,7 +13,7 @@ export const useColorStore = defineStore('colors', {
       this.colorInput = input
 
       // Finding hex without alpha
-      const regex = /#[0-9a-f]{6}|#[0-9a-f]{3}/ig
+      const regex = /#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3}/ig
       // Finding hex colors with alpha
       //const regex = /#([A-F0-9]{3,4}|[A-F0-9]{6}|[A-F0-9]{8})/ig
       const foundColors = input.match(regex)
@@ -21,6 +21,7 @@ export const useColorStore = defineStore('colors', {
       this.colorInputParsed = foundColors.map((i) => {
         return {
           hex: i,
+          asWritten: i.toLowerCase(),
         }
       })
     },
